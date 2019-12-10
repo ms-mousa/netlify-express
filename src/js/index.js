f1d88c50a32585ab328279bd181154c0ce188bb0
Matthew Sandoval <mat24c@gmail.com>
2019-12-10 11:39:01 -0800

    Fixed landingpage for mobile

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+let isOpen = false;
+
+document.getElementById('hamburger-menu').addEventListener('click', openMenu);
+
+function openMenu() {
+  isOpen = !isOpen;
+  if(isOpen) {
+    document.getElementById("hamburger-menu-img").src = "./img/menu_X.png";
+    document.getElementById("hamburger-menu-img").style.cssFloat = "left";
+    document.querySelector(".menu").style.display = 'flex';
+  } else {
+    document.getElementById("hamburger-menu-img").src = "./img/menu_Ham.png";
+    document.querySelector(".menu").style.display = 'none';
+  }
+}
