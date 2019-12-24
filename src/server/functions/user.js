31c4de44b1dbf3a1a6e93224a88e12e16f8d25e2
ms-mousa <m.shehata91@gmail.com>
2019-12-25 10:45:35 +1100

    try functions 2

--- src/server/functions/user.js
+++ src/server/functions/user.js
@@ -1,7 +1,34 @@
-exports.handler = async event => {
-  const subject = event.queryStringParameters.name || "World";
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}!`
-  };
-};
+("use strict");
+// import express mongoose and body parser
+const express = require("express");
+const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
+const db = require("mongoose");
+// initialize the server as an express object
+const server = express();
+
+// Import URI to connect to database from the secrets file
+const URI = require("../secrets").dbURI;
+
+// use the imported URI to connect to the db
+db.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connection to db successful"))
+  .catch(err => console.log(err));
+// use the bodyparser as an app level middlerware function
+server.use(bodyParser.json());
+
+// import the user model
+const User = require("../models/User");
+
+// @route   GET api Users
+// @desc    Get all the Users
+// @access  Public
+server.get("/user", (req, res) => {
+  User.find()
+    .sort({ date: -1 })
+    .then(users => res.json(users));
+});
+
+// export the serverless version of the server for netlify
+module.exports = server;
+module.exports.handler = serverless(server);
